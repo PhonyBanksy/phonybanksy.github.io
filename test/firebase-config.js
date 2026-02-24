a643aafb300fb93cb5ddf4f51a2ed392980911d6
PhonyBanksy <108962382+PhonyBanksy@users.noreply.github.com>
2026-02-24 17:14:55 +0000

    Update firebase-config.js

--- test/firebase-config.js
+++ test/firebase-config.js
@@ -1,24 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp }               from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore }                from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAwcSXBzanQt4v_vOaFJ_0srcExJhd2grk",
-  authDomain: "okias-events.firebaseapp.com",
-  projectId: "okias-events",
-  storageBucket: "okias-events.firebasestorage.app",
+  apiKey:            "AIzaSyAwcSXBzanQt4v_vOaFJ_0srcExJhd2grk",
+  authDomain:        "okias-events.firebaseapp.com",
+  projectId:         "okias-events",
+  storageBucket:     "okias-events.firebasestorage.app",
   messagingSenderId: "35652122565",
-  appId: "1:35652122565:web:af16d44360ed066baebdac"
+  appId:             "1:35652122565:web:af16d44360ed066baebdac"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore }               from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-const app  = initializeApp(firebaseConfig);
 
 export const auth     = getAuth(app);
 export const db       = getFirestore(app);
